a6c9c02e512c4ca0576c044b783bf0edcb5a2d52
Ian Epperson <ian@epperson.com>
2020-05-12 16:53:19 -0700

    Fix the startup script

--- serve.sh
+++ serve.sh
@@ -5,14 +5,6 @@ set -e
 
 export ACME=/root/.acme.sh/acme.sh
 
-$ACME --issue -d $DOMAIN_NAME \
-      -w /usr/share/nginx/html \
-      --reloadcmd "/install_cert.sh" \
-      --pre-hook "rm $OK_FILE" \
-      --cert-file /root/cert.pem \
-      --key-file /root/key.pem
-
-
 # for notifications, see https://github.com/acmesh-official/acme.sh/wiki/notify
 # the --notify-hook updates the config file with the contents of the env variables
 
@@ -32,5 +24,16 @@ if [ -n "$IFTTT_API_KEY" ];
   then $ACME --set-notify  --notify-hook ifttt
 fi;
 
-# Start the web server
-nginx -g 'daemon off';
+# Start the web server in the background
+nginx -g 'daemon off;' &
+
+# Run the cert using nginx
+$ACME --issue -d $DOMAIN_NAME \
+      -w /usr/share/nginx/html \
+      --reloadcmd "/install_cert.sh" \
+      --pre-hook "rm $OK_FILE" \
+      --cert-file /root/cert.pem \
+      --key-file /root/key.pem
+
+# Bring the web server back to the foreground
+fg
